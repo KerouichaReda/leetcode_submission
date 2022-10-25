1d2f5b228db622192306c9a125cfeeccb403fc68
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2022-10-25 17:08:18 +0800

    Update 434_Number_of_Segments_in_a_String.cxx

---
+++ submission/434_Number_of_Segments_in_a_String.cxx
@@ -0,0 +1,13 @@
+    
+    #include <string>
+    #include <iostream>
+    int countSegments(std::string& s) {
+        s.push_back(' ');
+        char pre = ' ';
+        int solution{0};
+        for(char& c : s){
+            if(c == ' ' && pre != ' ') solution++;
+            pre = c;
+        }
+        return solution;
+    }
