cf02e462dc73aa9eacf264dedea82ea9ebe8dc46
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2022-10-25 10:31:06 +0800

    Update settings.json and 1662_Check_If_Two_String_Arrays_are_Equivalent.cxx

---
+++ submission/1662_Check_If_Two_String_Arrays_are_Equivalent.cxx
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+bool arrayStringsAreEqual(std::vector<std::string>& word1,
+                          std::vector<std::string>& word2) {
+    return std::accumulate(word1.begin(), word1.end(), std::string{}) ==
+           std::accumulate(word2.begin(), word2.end(), std::string{});
+}
