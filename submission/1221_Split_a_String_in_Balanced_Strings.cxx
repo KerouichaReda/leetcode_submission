e055879b469175bfa5133cc721eab39ed955f562
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2022-10-28 14:12:57 +0800

    Update 1221_Split_a_String_in_Balanced_Strings.cxx

--- submission/1221_Split_a_String_in_Balanced_Strings.cxx
+++ submission/1221_Split_a_String_in_Balanced_Strings.cxx
@@ -1,12 +1,20 @@
-    int balancedStringSplit(string s) {
-        int counter[2] = {};
-        int solution = 0;
-        for(char& c  :s){
-            counter[c == 'L']++;
-            if(counter[0] == counter[1]){
-                solution++;
-                counter[0] = counter[1] = 0; 
-            }
+#include <string>
+#include <iostream>
+int balancedStringSplit(std::string s) {
+    int counter[2] = {};
+    int solution = 0;
+    for (char& c : s) {
+        counter[c == 'L']++;
+        if (counter[0] == counter[1]) {
+            solution++;
+            counter[0] = counter[1] = 0;
         }
-        return solution;        
     }
+    return solution;
+}
+
+int main(int argc, char const* argv[]) {
+    std::string s{"LLRR"};
+    std::cout << balancedStringSplit( s) << std::endl; 
+    return 0;
+}
