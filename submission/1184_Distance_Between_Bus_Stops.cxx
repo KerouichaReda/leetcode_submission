c17e91353eb81667e00abd4ec04e19dec0d78e69
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2022-10-31 17:40:34 +0800

    Update 1184_Distance_Between_Bus_Stops.cxx and 1385_Find_the_Distance_Value_Between_Two_Arrays.cxx

---
+++ submission/1184_Distance_Between_Bus_Stops.cxx
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int distanceBetweenBusStops(std::vector<int>& distance, int start,
+                            int destination) {
+    int size = distance.size();
+    int move[2] = {};
+    int solution[2] = {};
+
+    for (int i = 0; i < size; ++i) {
+        int lr = (start + i) % size;
+        int rl = (start - i + size) % size;
+        std::cout << distance[lr] << " " << distance[rl] << std::endl;
+        move[0] += distance[lr];
+        move[1] += distance[rl];
+        if (lr == destination) solution[0] = move[0];
+        if (rl == destination) solution[1] = move[1];
+    }
+    return std::min(solution[0], solution[1]);
+}
+
+int main(int argc, char const *argv[])
+{
+    std::vector<int> distance{1,2,3,4};
+    int start = 0;
+    int destination =1;
+    std::cout << distanceBetweenBusStops(distance,start, destination) << std::endl;
+
+    return 0;
+}
