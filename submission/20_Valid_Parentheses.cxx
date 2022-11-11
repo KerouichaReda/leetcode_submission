5e13cde689bfbd291715677f0340402fa2c613c0
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2022-11-11 13:56:10 +0800

    Update 20_Valid_Parentheses.cxx and 26_Remove_Duplicates.cxx

--- submission/20_Valid_Parentheses.cxx
+++ submission/20_Valid_Parentheses.cxx
@@ -1,48 +1,42 @@
 /*
  * 20_Valid_Parentheses.cxx
- * 
+ *
  * Copyright 2021 RedaKerouicha <redakerouicha@localhost>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
-
 
 #include <iostream>
 #include <string>
 
-bool isValid(std::string s) {
-	// support variables
-	char arr[s.size()];
-	int pos = 0;
-	for (char c: s) {
-		if (c == '(' || c == '[' || c == '{') {
-			arr[pos++] = c;
-		}
-		else if (pos && abs(arr[pos - 1] - c) < 3) pos--;
-		else return false;
-	}
-	// checking if all the parentheses are now 0
-	return !pos;
+bool isValid(std::string& s) {
+    std::string stack;
+    for (char& c : s) {
+        if (c == '(' || c == '[' || c == '{') {
+            stack.push_back(c);
+        } else if (!stack.empty() &&
+                   (c == (stack.back() + 1) || c == stack.back() + 2)) {
+            stack.pop_back();
+        } else {
+            return false;
+        }
+    }
+    return stack.empty();
 }
 
-int main(int argc, char **argv)
-{
-	
-	return 0;
-}
-
+int main(int argc, char** argv) { return 0; }
--- submission/26_Remove_Duplicates.cxx
+++ submission/26_Remove_Duplicates.cxx
@@ -1,47 +1,43 @@
 /*
  * 26_Remove_Duplicates.cxx
- * 
+ *
  * Copyright 2020 RedaKerouicha <redakerouicha@localhost>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
-
 
 #include <iostream>
 #include <vector>
 
 int removeDuplicates(std::vector<int>& nums) {
-    int i = !nums.empty();    
-    for (int n : nums)
-        if (n > nums[i-1])
-            nums[i++] = n;
+    int i = !nums.empty();
+    for (int& n : nums)
+        if (n > nums[i - 1]) nums[i++] = n;
     return i;
 }
 
-int main(int argc, char **argv)
-{
-	std::vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
-	int result = removeDuplicates(nums);
-	
-	for(int i=0 ;i<result;++i){
-		std::cout<<nums[i]<<" ";
-	}
-	std::cout<<std::endl;
-	return 0;
-}
+int main(int argc, char** argv) {
+    std::vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    int result = removeDuplicates(nums);
 
+    for (int i = 0; i < result; ++i) {
+        std::cout << nums[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
