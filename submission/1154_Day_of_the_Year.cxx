5b1a16b0ada85f40a2b8e42bb14455873f31c3a2
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2022-11-16 13:06:42 +0800

    Update 1154_Day_of_the_Year.cxx

---
+++ submission/1154_Day_of_the_Year.cxx
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+const int months[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int dayOfYear(std::string& date) {
+    int year = std::stoi(date.substr(0, 4));
+    int month = std::stoi(date.substr(5, 2));
+    int day = std::stoi(date.substr(8, 2));
+    for (int i = 0; i < month - 1; ++i) {
+        day += months[i];
+    }
+    day += (month > 2 && (year - 1900) % 4 == 0 && year != 1900);
+    return day;
+}
+int main(int argc, char const* argv[]) {
+    /* code */
+    return 0;
+}
