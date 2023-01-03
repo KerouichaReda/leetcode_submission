3ff798f3f64019771f9b14bd3a87f401cb442c09
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2023-01-03 16:59:09 +0800

    Update 290_Word_Pattern.cxx

--- submission/290_Word_Pattern.cxx
+++ submission/290_Word_Pattern.cxx
@@ -1,89 +1,63 @@
 /*
  * 290_Word_Pattern.cxx
- * 
+ *
  * Copyright 2020 RedaKerouicha <redakerouicha@localhost>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
+ *
  * https://leetcode.com/problems/word-pattern/
  */
 
-
 #include <iostream>
-#include <sstream>
-/*
- * map<char, int> p2i;
-    map<string, int> w2i;
-    istringstream in(str);
-    int i = 0, n = pattern.size();
-    for (string word; in >> word; ++i) {
-        if (i == n || p2i[pattern[i]] != w2i[word])
-            return false;
-        p2i[pattern[i]] = w2i[word] = i + 1;
-    }
-    return i == n;
-}*/
- bool wordPattern(string pattern, string str) 
-    {
-       std::stringstream input(str);
-        std::string words[26],word;
-        
-        int space=0;
-        bool test[26];
-        for(int i=0;i<26;i++)
-        {
-            test[i]=false;
-        }
-        for(int i=0;i<str.length();i++)
-        {
-            if(str[i]==' ')
-                space++;
-        }
-        if((space+1)!=(int)pattern.length())
-            return false;
-        for (int i=0;i<pattern.length();i++)
-        {   
-            input>>word;
-            if(!test[pattern[i]-'a'])
-            {
-                for(int j=0;j<26;j++)
-                {
-                    if(words[j]==word)
-                        return false;
-                }
-                words[pattern[i]-'a']=word;
-                test[pattern[i]-'a']=true;
-            }
-            else
-            {
-                if(words[pattern[i]-'a']!=word)
-                {
-                    return false;
-                }
-            }
-        }
+#include <string>
+#include <vector>
+#include <unordered_map>
 
-        return true;
+bool wordPattern(std::string pattern, std::string s) {
+    std::vector<std::string> tokens;
+    s.push_back(' ');
+    std::string temp;
+    for (char& c : s) {
+        if (c == ' ') {
+            if (!temp.empty()) {
+                tokens.push_back(temp);
+                temp.clear();
+            }
+        } else {
+            temp.push_back(c);
+        }
     }
-int main(int argc, char **argv)
-{
-	std::string pattern="abba";
-	std::string str="dog cat cat dog";
-	std::cout<<wordPattern(pattern,str)<<std::endl;
-	
-	return 0;
+    if (pattern.size() != tokens.size()) return false;
+    std::unordered_map<char, std::string> m1;
+    std::unordered_map<std::string, char> m2;
+    for (std::size_t index = 0, size = pattern.size(); index < size; index++) {
+        if (m1.count(pattern[index]) && m1[pattern[index]] != tokens[index])
+            return false;
+        if (m2.count(tokens[index]) && m2[tokens[index]] != pattern[index])
+            return false;
+        m1[pattern[index]] = tokens[index];
+        m2[tokens[index]] = pattern[index];
+    }
+    return true;
 }
 
+int main(int argc, char** argv) {
+    std::string pattern = "abba";
+    std::string str = "dog cat cat dog";
+    std::cout << wordPattern(pattern, str) << std::endl;
+
+    return 0;
+}
