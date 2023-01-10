97943b9de9c8347b5b3c72eb85bb09b845cb38b8
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2023-01-10 09:52:46 +0800

    Update 100_Same_Tree.cxx

--- submission/100_Same_Tree.cxx
+++ submission/100_Same_Tree.cxx
@@ -1,16 +1,17 @@
-//https://leetcode.com/problems/same-tree/
+// https://leetcode.com/problems/same-tree/
 struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
 };
 bool isSameTree(TreeNode* rootP, TreeNode* rootQ) {
-return  rootP == nullptr && rootQ == nullptr ? true :
-        rootP == nullptr || rootQ == nullptr ? false :
-        rootP->val == rootQ->val ? 
-        isSameTree(rootP->left,rootQ->left) && isSameTree(rootP->right,rootQ->right) : 
-        false; 
+    return rootP == nullptr || rootQ == nullptr
+               ? rootQ == rootP
+               : rootP->val == rootQ->val &&
+                     isSameTree(rootP->left, rootQ->left) &&
+                     isSameTree(rootP->right, rootQ->right);
 }
