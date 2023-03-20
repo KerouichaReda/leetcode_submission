fe591e0ae644462b9a2e3cb81d46788345b29248
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2023-03-20 13:43:07 +0800

    Update 605_Can_Place_Flowers.cxx

--- submission/605_Can_Place_Flowers.cxx
+++ submission/605_Can_Place_Flowers.cxx
@@ -1,18 +1,17 @@
 #include <vector>
 
-bool canPlaceFlowers(std::vector<int>& flowerbed, int n) {
+bool canPlaceFlowers(vector<int>& flowerbed, int n) {
     flowerbed.push_back(0);
     flowerbed.insert(flowerbed.begin(), 0);
     for (int i = 2, size = flowerbed.size(); i < size && n > 0; ++i) {
-        n -= ((flowerbed[i] + flowerbed[i - 1] + flowerbed[i - 2]) == 0);
-        flowerbed[i - 1] |=
-            (flowerbed[i] + flowerbed[i - 1] + flowerbed[i - 2]) == 0;
+        if ((flowerbed[i] + flowerbed[i - 1] + flowerbed[i - 2]) == 0) {
+            n--;
+            flowerbed[i - 1] = 1;
+        }
     }
-    return n <= 0;
+    return n == 0;
 }
-
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const* argv[]) {
     /* code */
     return 0;
 }
