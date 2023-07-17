e81de04b33f6ee1777a24b29ebb714a9000deb08
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2023-07-17 17:37:48 +0800

    Update 445_Add_Two_Numbers_II.cxx

--- submission/445_Add_Two_Numbers_II.cxx
+++ submission/445_Add_Two_Numbers_II.cxx
@@ -1,76 +1,65 @@
 /*
  * 445_Add_Two_Numbers_II.cxx
- * 
+ *
  * Copyright 2020 RedaKerouicha <redakerouicha@localhost>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 
-
 #include <iostream>
-#include <stack>
+
 
 #include "listnode.hpp"
 
-
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-        std::stack<int> sl1;
-        std::stack<int> sl2;
-        
-        while(l1!=nullptr){
-            sl1.push(l1->val);
-            l1=l1->next;
-        }
-         while(l2!=nullptr){
-            sl2.push(l2->val);
-            l2=l2->next;
-        }
-        
-        ListNode* temp =nullptr;
-        int r=0;
-        
-        while (!sl1.empty() || !sl2.empty() || r>0){
-            
-            if (sl1.empty() && sl2.empty()){
-                temp = new ListNode (r,temp);
-                r=0;
-            }else if (sl1.empty()) {                
-                temp = new ListNode ((sl2.top()+ r)%10,temp);
-                r= (sl2.top()+ r)/10;
-                sl2.pop();
-            }else if (sl2.empty()){
-                temp = new ListNode ((sl1.top()+ r)%10,temp);
-                r= (sl1.top()+ r)/10;
-                sl1.pop();
-            } 
-            else {
-                temp = new ListNode ((sl1.top()+sl2.top()+ r)%10,temp);
-                r= (sl1.top()+sl2.top()+ r)/10;
-                sl2.pop();
-                sl1.pop();
-            }
-        }
-        return temp;        
+ListNode* reverse(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) return head;
+    ListNode* pre = nullptr;
+    ListNode* next;
+    while (head != nullptr) {
+        next = head->next;
+        head->next = pre;
+        pre = head;
+        head = next;
     }
-int main(int argc, char **argv)
-{
-	
-	return 0;
+    return pre;
 }
-
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    if (l1 == nullptr || l2 == nullptr) return l1 == nullptr ? l1 : l2;
+    ListNode solution(0);
+    ListNode* curr = &solution;
+    l1 = reverse(l1);
+    l2 = reverse(l2);
+    int rem = 0, n1, n2;
+    while (l1 != nullptr || l2 != nullptr || rem) {
+        if (l1 != nullptr) {
+            n1 = l1->val;
+            l1 = l1->next;
+        }
+        if (l2 != nullptr) {
+            n2 = l2->val;
+            l2 = l2->next;
+        }
+        curr->next = new ListNode((rem + n1 + n2) % 10);
+        curr = curr->next;
+        rem = (rem + n1 + n2) / 10;
+        n1 = 0;
+        n2 = 0;
+    }
+    return reverse(solution.next);
+}
+int main(int argc, char** argv) { return 0; }
