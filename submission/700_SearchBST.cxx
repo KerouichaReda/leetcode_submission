74e043dbc7ba07ce0ddcb1e8c72c5e046a674b6b
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2023-07-21 15:56:35 +0800

    Update 700_SearchBST.cxx

--- submission/700_SearchBST.cxx
+++ submission/700_SearchBST.cxx
@@ -1,46 +1,34 @@
 /*
  * 700_SearchBST.cxx
- * 
+ *
  * Copyright 2020 RedaKerouicha <redakerouicha@localhost>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 
-
+#include <treenode.hpp>
 #include <iostream>
 
 TreeNode* searchBST(TreeNode* root, int val) {
-	if(root != nullptr ){
-		if(root->val == val)
-			return root;
-		else if (root->val < val){
-			return searchBST(root->right, val) ;
-		}
-		else {
-			return searchBST(root->left, val) ;
-		}
-	}
-	return nullptr;        
+    return root == nullptr || root->val == val
+               ? root
+               : root->val < val ? searchBST(root->right, val)
+                                 : searchBST(root->left, val);
 }
 
-int main(int argc, char **argv)
-{
-	
-	return 0;
-}
-
+int main(int argc, char** argv) { return 0; }
