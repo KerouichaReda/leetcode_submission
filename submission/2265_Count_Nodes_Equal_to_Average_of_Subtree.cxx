92ff7cf8cddafd2f4e244056938942b992814cb0
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2023-11-02 14:46:42 +0800

    Update 2265_Count_Nodes_Equal_to_Average_of_Subtree.cxx

---
+++ submission/2265_Count_Nodes_Equal_to_Average_of_Subtree.cxx
@@ -0,0 +1,21 @@
+#include "treenode.hpp"
+struct temp {
+    temp() {}
+    temp(temp left, temp right, int& val)
+        : sum(left.sum + right.sum + val),
+          count(left.count + right.count + 1),
+          solution(left.solution + right.solution + (val == (sum / count))) {}
+    int sum{0};
+    int count{0};
+    int solution{0};
+};
+int averageOfSubtree(TreeNode* root) { return helper(root).solution; }
+temp helper(TreeNode* root) {
+    return root == nullptr ? temp() : temp(helper(root->left),
+                                           helper(root->right), root->val);
+}
+
+int main(int argc, char const* argv[]) {
+    /* code */
+    return 0;
+}
