aa7af35ce794c07a92267e71445d7cacf4205bda
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2024-01-03 14:55:24 +0800

    Update 2125_Number_of_Laser_Beams_in_a_Bank.cxx

---
+++ submission/2125_Number_of_Laser_Beams_in_a_Bank.cxx
@@ -0,0 +1,21 @@
+#include <vector>
+#include <numeric>
+#include <string>
+#include <iostream>
+int numberOfBeams(std::vector<std::string>& bank) {
+    int solution{0};
+    int pre{0};
+    for (std::string& s : bank) {
+        int curr = std::accumulate(s.begin(), s.end(), 0, [](int sum, char c) {
+            return sum + (c == '1');
+        });
+        solution += curr * pre;
+        pre = curr ? curr : pre;
+    }
+    return solution;
+}
+
+int main(int argc, char const* argv[]) {
+    /* code */
+    return 0;
+}
