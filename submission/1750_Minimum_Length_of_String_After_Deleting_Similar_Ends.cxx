e925fcf8afd48f7548f82cd19a9738345fd0c0e2
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2024-03-05 14:28:55 +0800

    Update 1750_Minimum_Length_of_String_After_Deleting_Similar_Ends.cxx

---
+++ submission/1750_Minimum_Length_of_String_After_Deleting_Similar_Ends.cxx
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+int minimumLength(std::string s) {
+    int i = 0;
+    int j = s.size() - 1;
+    for (; i < j && s[i] == s[j];) {
+        char c = s[i];
+        for (; i <= j && s[i] == c; i++);
+        for (; i <= j && s[j] == c; j--);
+    }
+    return j - i + 1;
+}
+
+int main(int argc, char const* argv[]) {
+    std::string s = "aba";
+    std::cout << minimumLength(s) << std::endl;
+    return 0;
+}
