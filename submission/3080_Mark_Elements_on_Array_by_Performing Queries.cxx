3c3717c807f2e3d3b74f2c2431e4f9fe3976be83
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2024-03-22 10:14:50 +0800

    Update 3080_Mark_Elements_on_Array_by_Performing Queries.cxx

---
+++ submission/3080_Mark_Elements_on_Array_by_Performing Queries.cxx
@@ -0,0 +1,45 @@
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+
+std::vector<long long> unmarkedSumArray(
+    std::vector<int>& nums, std::vector<std::vector<int>>& queries) {
+    std::map<int, std::vector<int>> m;
+    std::set<int> marked;
+    std::int64_t sum{0};
+    std::vector<long long> solution;
+    for (int i = nums.size() - 1; i >= 0; --i) {
+        m[nums[i]].push_back(i);
+        sum += nums[i];
+    };
+    for (std::vector<int>& q : queries) {
+        int index = q[0];
+        int k = q[1];
+        if (!marked.count(index)) {
+            sum -= nums[index];
+            marked.insert(index);
+        }
+        for (int i = 0; !m.empty() && i < k; i++) {
+            int k_index = (*m.begin()).second.back();
+            (*m.begin()).second.pop_back();
+            if ((*m.begin()).second.empty()) {
+                m.erase(m.begin());
+            }
+            if (marked.count(k_index)) {
+                i--;
+                continue;
+            }
+            marked.insert(k_index);
+            sum -= nums[k_index];
+        }
+        solution.push_back(sum);
+    }
+    return solution;
+}
+
+int main(int argc, char const* argv[]) {
+    /* code */
+    return 0;
+}
