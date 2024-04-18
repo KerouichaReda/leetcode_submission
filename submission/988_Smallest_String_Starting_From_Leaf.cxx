f326755d237b095f310efe77a1a8205a37c47b05
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2024-04-18 17:38:28 +0800

    Update 988_Smallest_String_Starting_From_Leaf.cxx

--- submission/988_Smallest_String_Starting_From_Leaf.cxx
+++ submission/988_Smallest_String_Starting_From_Leaf.cxx
@@ -1,2 +1,20 @@
 #include "treenode.hpp"
 #include <string>
+
+std::string smallestFromLeaf(TreeNode* root, std::string temp = "") {
+    if (root == nullptr) return temp;
+    temp = std::string(1, root->val + 'a') + temp;
+    if (root->left == nullptr && root->right == nullptr) return temp;
+    std::string l = smallestFromLeaf(root->left, temp);
+    std::string r = smallestFromLeaf(root->right, temp);
+    return root->left != nullptr && root->right != nullptr
+               ? std::min(l, r)
+               : root->right != nullptr ? r : l;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
