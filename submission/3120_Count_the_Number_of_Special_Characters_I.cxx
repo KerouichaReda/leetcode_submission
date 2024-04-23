424be284becbfe2cb95a1ca62bf70279c73cad42
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2024-04-23 15:46:32 +0800

    Update 3120_Count_the_Number_of_Special_Characters_I.cxx

---
+++ submission/3120_Count_the_Number_of_Special_Characters_I.cxx
@@ -0,0 +1,17 @@
+#include <string>
+#include <set>
+#include <iostream>
+int numberOfSpecialChars(std::string word) {
+    std::set<char> s(word.begin(), word.end());
+    int solution{};
+    for (int i = 0, size = 26; i < size; ++i) {
+        solution += s.count('a' + i) && s.count('A' + i);
+    }
+    return solution;
+}
+
+int main(int argc, char const* argv[]) {
+    std::string word{"abc"};
+    std::cout << numberOfSpecialChars(word) << std::endl;
+    return 0;
+}
