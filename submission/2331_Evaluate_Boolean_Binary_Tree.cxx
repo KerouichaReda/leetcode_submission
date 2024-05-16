fe6e69add9d52ab1778a38178bf6e0c6259a5d20
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2024-05-16 09:53:00 +0800

    Update 2331_Evaluate_Boolean_Binary_Tree.cxx

---
+++ submission/2331_Evaluate_Boolean_Binary_Tree.cxx
@@ -0,0 +1,18 @@
+#include "treenode.hpp"
+
+bool evaluateTree(TreeNode* root) {
+    return root == nullptr ? true
+                           : (root->left == nullptr && root->right == nullptr)
+                                 ? root->val == 1
+                                 : root->val == 2
+                                       ? evaluateTree(root->left) ||
+                                             evaluateTree(root->right)
+                                       : evaluateTree(root->left) &&
+                                             evaluateTree(root->right);
+}
+
+int main(int argc, char const* argv[]) {
+    TreeNode* root = new TreeNode();
+    std::cout << evaluateTree(root) << std::endl;
+    return 0;
+}
