5e87bba9d4f8771a56cdceb5c7aae720f850670a
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2024-05-17 10:05:06 +0800

    Update LICENSE and 1325_Delete_Leaves_With_a_Given_Value.cxx

--- submission/1325_Delete_Leaves_With_a_Given_Value.cxx
+++ submission/1325_Delete_Leaves_With_a_Given_Value.cxx
@@ -1,47 +1,36 @@
 /*
  * 1325_Delete_Leaves_With_a_Given_Value.cxx
- * 
- * Copyright 2021 RedaKerouicha <redakerouicha@localhost>
- * 
+ *
+ * Copyright 2024 RedaKerouicha <redakerouicha@localhost>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 
-
 #include <iostream>
-TreeNode* removeLeafNodes(TreeNode* root,const int &target) {
-	if(root == nullptr){
-		root = nullptr;
-	}else  {
-		if(root->left != nullptr){                
-			root->left = removeLeafNodes(root->left, target);
-		}            
-		if(root->right != nullptr){
-			root->right = removeLeafNodes(root->right, target);            
-		}
-	}
-	return isLeaf(root,target) ? nullptr : root;        
-}    
-constexpr bool isLeaf(TreeNode* root,const int &target){
-	return root == nullptr ? false : root->left == nullptr && root->right == nullptr && root->val == target;
+#include "treenode.hpp"
+TreeNode* removeLeafNodes(TreeNode* root, int target) {
+    if (root == nullptr) return root;
+    root->left = removeLeafNodes(root->left, target);
+    root->right = removeLeafNodes(root->right, target);
+    return (root->left == root->right && root->val == target) ? nullptr : root;
 }
-int main(int argc, char **argv)
-{
-	
-	return 0;
+int main(int argc, char** argv) {
+    TreeNode* root = new TreeNode(1);
+    removeLeafNodes(root, 1);
+    return 0;
 }
-
