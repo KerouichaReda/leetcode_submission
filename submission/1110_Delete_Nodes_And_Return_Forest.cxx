0e04946852f750f8abec58eb8cafc80de9a03810
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2024-07-17 13:50:15 +0800

    Update 820127.820173.pdf, 1008_Construct_BST_ Preorder_Traversal.cxx, and 1110_Delete_Nodes_And_Return_Forest.cxx

--- submission/1110_Delete_Nodes_And_Return_Forest.cxx
+++ submission/1110_Delete_Nodes_And_Return_Forest.cxx
@@ -1,51 +1,37 @@
 /*
- * 1008_Construct_BST_ Preorder_Traversal.cxx
- * 
- * Copyright 2021 RedaKerouicha <redakerouicha@localhost>
- * 
+ * 1110_Delete_Nodes_And_Return_Forest.cxx
+ *
+ * Copyright 2024 - present RedaKerouicha
+ * <redakerouicha@kerouicha.reda@gmail.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 
-
-#include <iostream>
+#include <set>
 #include <vector>
+#include <iostream>
 #include "treenode.hpp"
-
-
-TreeNode* bstFromPreorder(vector<int>& preorder) {
-	int index = 0;
-	return  bstFromPreorder_util(preorder, index, INT_MAX );
-	
-}
-TreeNode* bstFromPreorder_util(vector<int>& preorder, int &index ,int highest) {
-	if(index >= preorder.size() || preorder[index] > highest){
-		return nullptr;
-	}
-	TreeNode* root = new TreeNode(preorder[index++]);
-	root->left = bstFromPreorder_util(preorder, index,root->val);
-	root->right = bstFromPreorder_util(preorder, index , highest);         
-	return root;
-	
+std::vector<TreeNode*> delNodes(TreeNode* root, std::vector<int>& to_delete) {
+    std::set<int> s;
 }
 
-int main(int argc, char **argv)
-{
-	
-	return 0;
+int main(int argc, char const* argv[]) {
+    TreeNode* root = new TreeNode(1);
+    std::vector<TreeNode*> solution = delNodes(root, {1, 2, 3});
+    return 0;
 }
-
