6dcce8b04ae71e5f07f7cc1574a79c71b5aa6885
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2025-04-11 15:32:09 +0800

    Update 1837_sum_of_digits_in_base_k.cxx

---
+++ submission/1837_sum_of_digits_in_base_k.cxx
@@ -0,0 +1,44 @@
+/*
+ * 1837_sum_of_digits_in_base_k.cxx
+ *
+ * Copyright 2025 - present RedaKerouicha <reda_kerouicha@outlook.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+#include <map>
+#include <numeric>
+#include <algorithm>
+
+int sumBase(int n, int k) {
+    int result{0};
+    while (n) {
+        result += n % k;
+        n /= k;
+    }
+    return result;
+}
+
+int main(int argc, char** argv) {
+    std::cout << sumBase(56, 6) << std::endl;
+    return 0;
+}
