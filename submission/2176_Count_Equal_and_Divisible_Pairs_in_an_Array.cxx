3ab42cad3ee585e779a0984a0c874ccd2776f9a5
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2025-04-17 09:59:51 +0800

    Update settings.json and 2176_Count_Equal_and_Divisible_Pairs_in_an_Array.cxx

--- submission/2176_Count_Equal_and_Divisible_Pairs_in_an_Array.cxx
+++ submission/2176_Count_Equal_and_Divisible_Pairs_in_an_Array.cxx
@@ -1,21 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
+/*
+ * 2176_count_equal_and_divisible_pairs_in_an_array.cxx
+ *
+ * Copyright 2025 - present RedaKerouicha <reda_kerouicha@outlook.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
 
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+#include <map>
+#include <numeric>
+#include <algorithm>
 int countPairs(std::vector<int>& nums, int k) {
-    std::unordered_map<int, std::vector<int>> m;
-    int solution{0};
-    for (int i = nums.size() - 1; i >= 0; --i) {
-        for (int index : m[nums[i]]) {
-            solution += ((i * index) % k == 0);
+    int solution{};
+    for (std::size_t i = 0, size = nums.size(); i < size - 1; i++) {
+        for (std::size_t j = i + 1; j < size; j++) {
+            solution += (nums[i] == nums[j] && i * j % k == 0);
         }
-        m[nums[i]].push_back(i);
     }
     return solution;
 }
-
-int main(int argc, char const* argv[]) {
-    /* code */
+int main(int argc, char** argv) {
+    std::vector<int> nums{1, 2, 3, 4, 5};
+    std::cout << countPairs(nums, 2) << std::endl;
     return 0;
 }
-     
