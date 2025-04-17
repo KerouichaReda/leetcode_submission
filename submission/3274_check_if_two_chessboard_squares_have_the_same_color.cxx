83065f3498257ccff68c29d2ea06a7037a0228da
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2025-04-17 15:06:57 +0800

    Update 3274_check_if_two_chessboard_squares_have_the_same_color.cxx

---
+++ submission/3274_check_if_two_chessboard_squares_have_the_same_color.cxx
@@ -0,0 +1,33 @@
+/*
+ * 3274_check_if_two_chessboard_squares_have_the_same_color.cxx
+ *
+ * Copyright 2025 - present RedaKerouicha <reda_kerouicha@outlook.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+ 
+ #include <iostream>
+ #include <set>
+ #include <string>
+ #include <vector>
+ #include <map>
+ #include <numeric>
+ #include <algorithm>
+ 
+ int main(int argc, char** argv) { return 0; }
+    
