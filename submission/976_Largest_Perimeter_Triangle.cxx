995550706446f25defae8d51b3cc2e06043b7835
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2025-08-24 23:00:12 +0800

    0976. Largest Perimeter Triangle

--- submission/976_Largest_Perimeter_Triangle.cxx
+++ submission/976_Largest_Perimeter_Triangle.cxx
@@ -1,24 +1,33 @@
-// https://leetcode.com/problems/largest-perimeter-triangle/
-
-// Given an integer array nums, return the largest perimeter of a triangle with
-// a non-zero area, formed from three of these lengths. If it is impossible to
-// form any triangle of a non-zero area, return 0.
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-int largestPerimeter(std::vector<int>& nums) {
-    std::sort(nums.begin(), nums.end(), std::greater());
-    for (size_t i = 0, size = nums.size() - 2; i < size; ++i) {
-        if (nums[i] < (nums[i + 1] + nums[i + 2])) {
-            return nums[i] + nums[i + 1] + nums[i + 2];
-        }
-    }
-    return 0;
-}
-
-int main(int argc, char const* argv[]) {
-    std::vector<int> nums{1, 2, 1, 4};
-    std::cout << largestPerimeter(nums) << std::endl;
-    return 0;
-}
+/*
+ * 976_largest_perimeter_triangle.cxx
+ *
+ * Copyright 2025 - present RedaKerouicha <reda_kerouicha@outlook.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+ 
+ #include <iostream>
+ #include <set>
+ #include <string>
+ #include <vector>
+ #include <map>
+ #include <numeric>
+ #include <algorithm>
+ 
+ int main(int argc, char** argv) { return 0; }
+    
